7452d0acfbc0772fac686d72b5d9bff7b723490c
quachv <v.quach@bellevuecollege.edu>
2018-05-27 22:39:16 -0700

    edits

--- artifact4java.js
+++ artifact4java.js
@@ -2,16 +2,24 @@ function ValidateForm(){
   var validFirstname = false;
   var letters = /^[A-Za-z]+$/;
   var validLastname = false;
+  var userEmail = document.getElementById("email").value;
+  var atpos = userEmail.indexOf("@");
+  var dotpos = userEmail.lastIndexOf(".");
+  var validEmail = false;
   var validPhone = false;
   var numbers = /^[0-9]+$/;
   var validUsername = false;
   var validUserPassword = false;
+  var validAddress = false;
+  var validCity = false;
+  var country = document.getElementById("countries").value;
+  var validZipcode = false;
   var errorMessages ="";  // All the error messages are going to stay in this variable
 
   /*********** VALIDATES FIRST NAME ******** */
   if (myContact.firstname.value===null || myContact.firstname.value===""|| myContact.firstname.value.length > 20 || !myContact.firstname.value.match(letters))
      errorMessages += "<p>The first name must be less than 20 characters and it is required. Only letters are accepted.</p>";
-     else
+	else
      validFirstname = true; 
 
   /*********** VALIDATES LAST NAME ******** */
@@ -23,7 +31,7 @@ function ValidateForm(){
   /*********** VALIDATES PHONE NUMBER ******** */
   if (myContact.phone.value===null || myContact.phone.value===""|| myContact.phone.value.length > 15 || !myContact.phone.value.match(numbers))
     errorMessages += "<p>The phone number must be less than 15 characters and it is required. Only numbers are accepted.</p>";
-  else
+	else
     validPhone = true; 
   
   /*********** VALIDATES USERNAME ******** */
@@ -38,8 +46,27 @@ function ValidateForm(){
      else
      validUserPassword = true; 
  
+
+  /*********** VALIDATES ADDRESS ******** */
+  if (myContact.address.value===null || myContact.address.value==="")
+    errorMessages += "<p>Address is required.</p>";
+  else
+    validAddress = true; 
+  
+/*********** VALIDATES CITY ******** */
+  if (myContact.city.value===null || myContact.city.value==="")
+    errorMessages += "<p>City is required.";
+  else
+    validCity = true; 
+  
+/*********** VALIDATES ZIP CODE ******** */
+  if (myContact.countries === "USA" || myContact.zipcode.value===null ||myContact.zipcode.value===""|| myContact.zipcode.value.length > 5)
+    errorMessages += "<p>Zip code must be maximum of 5 characters and is reqired. Only numbers are accepted.</p>";
+  else
+    validZipcode = true; 
+ 
   document.getElementById("errorMessages").innerHTML = errorMessages;
 
   // Make sure you return all the boolean variables that are checking each field
-  return (validFirstname && validLastname && validPhone && validUsername && validUserPassword) ;
+  return (validFirstname && validLastname && validEmail && validPhone && validUsername && validUserPassword && validAddress && validCity && validZipcode) ;
 }
